f1d8bd0e0e9bd715b8e96c404ad55058cd8982ec
AlexV713 <62916245+AlexV713@users.noreply.github.com>
2020-04-22 20:14:37 -0700

    Updates scripts
    The weather feeds wasn't showing up, so I moved it to the top

--- scripts.js
+++ scripts.js
@@ -8,6 +8,17 @@ function dropLinks() {
     }
 }
 
+// Weather Feed
+! function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://weatherwidget.io/js/widget.min.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }
+}(document, 'script', 'weatherwidget-io-js');
+
 // Slideshow
 
 var slideIndex = 1;
@@ -42,14 +53,3 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 };
-
-// Weather Feed
-! function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (!d.getElementById(id)) {
-        js = d.createElement(s);
-        js.id = id;
-        js.src = 'https://weatherwidget.io/js/widget.min.js';
-        fjs.parentNode.insertBefore(js, fjs);
-    }
-}(document, 'script', 'weatherwidget-io-js');
