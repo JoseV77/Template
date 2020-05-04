4f6a740401ea9cee1a755f2b2a696f9d8bbe0f78
AlexV713 <62916245+AlexV713@users.noreply.github.com>
2020-05-04 16:37:41 -0700

    Javascript bug fixes
    Just some javascript bug fixes.

--- scripts.js
+++ scripts.js
@@ -29,20 +29,15 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
-    slideIndex = 1
+    slideIndex = 1;
   }
   if (n < 1) {
-    slideIndex = slides.length
+    slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -52,4 +47,4 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-};
+}
