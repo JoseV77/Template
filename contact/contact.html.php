0af1a6ec4461754c8dc9bd6708469ace96d8890c
AlexV713 <62916245+AlexV713@users.noreply.github.com>
2020-05-06 17:50:06 -0700

    Added Contact HTML and edited index.html
    I addded the html for the contact page and moved the main tag.

---
+++ contact/contact.html.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/cf72e27b3d.js" crossorigin="anonymous"></script>
+    <script src="scripts.js" type="text/javascript"></script>
+    <title>Ace in the Hole Multisports Events</title>
+</head>
+
+<body>
+
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v6.0">
+    </script>
+
+    <main>
+
+        <header>
+            <a href="javascript:void(0);" class="icon" onclick="dropLinks()">
+                <i class="fas fa-bars"></i>
+            </a>
+
+            <nav id="desktop-nav">
+                <h1>Heading 1</h1>
+                <a href="index.html">Home</a>
+                <a href="events.html">Events</a>
+                <a href="register.html">Register</a>
+                <a href="contact.html">Contact</a>
+            </nav>
+
+            <nav id="mobile-nav">
+                <h1>Heading 1</h1>
+                <div id="links">
+                    <a href="index.html">Home</a>
+                    <a href="events.html">Events</a>
+                    <a href="register.html">Register</a>
+                    <a href="contact.html">Contact</a>
+                </div>
+            </nav>
+        </header>
+
+        <div id="wrapper">
+
+        </div>
+
+    </main>
+
+    <footer>
+        <p>Copyright &copy; 2020 Ace in the Hole Multisport Events</p>
+    </footer>
+
+</body>
+
+</html>
